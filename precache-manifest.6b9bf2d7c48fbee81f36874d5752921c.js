a0ce3279367989ca5c1ad87bf305296df9be674f
HR Bappy <mdhabiburrb@gmail.com>
2020-10-27 01:03:46 +0600

    Updates

---
+++ precache-manifest.6b9bf2d7c48fbee81f36874d5752921c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3bbface14705c58d1ca0aaf0d5bc21",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "1fd5044ffc4002036bf5",
+    "url": "/first-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "91e417f1fbc50c60e914",
+    "url": "/first-react/static/css/main.60498df5.chunk.css"
+  },
+  {
+    "revision": "1fd5044ffc4002036bf5",
+    "url": "/first-react/static/js/2.857311bd.chunk.js"
+  },
+  {
+    "revision": "5ec7a42b51d190c85194d1382b8d57c1",
+    "url": "/first-react/static/js/2.857311bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e417f1fbc50c60e914",
+    "url": "/first-react/static/js/main.744c3c3b.chunk.js"
+  },
+  {
+    "revision": "7d02a3bb369e62e751c9",
+    "url": "/first-react/static/js/runtime-main.3ccf10ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "18ff58ed079c8440bdcb9d137d7c3e95",
+    "url": "/first-react/static/media/nayeem.18ff58ed.jpg"
+  },
+  {
+    "revision": "71325a7c52772d150117f703cbad00ec",
+    "url": "/first-react/static/media/neela.71325a7c.jpg"
+  },
+  {
+    "revision": "8925b4b5b5df9eb788098468ee3089cd",
+    "url": "/first-react/static/media/rakhi.8925b4b5.jpg"
+  },
+  {
+    "revision": "197a7891b830f2cbe85aa7a10c80a459",
+    "url": "/first-react/static/media/shammah.197a7891.jpg"
+  }
+]);
